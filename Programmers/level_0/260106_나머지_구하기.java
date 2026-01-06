8d578ac083d4355b30d594d56a099258e8ca2d26
jahee24 <8dhfflavlr8@naver.com>
2026-01-06 10:09:33 +0900

    Solve:나머지 구하기

---
+++ Programmers/level_0/260106_나머지_구하기.java
@@ -0,0 +1,9 @@
+package level_0;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120810
+ class Solution120810{
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        answer = num1 % num2;
+        return answer;
+    }
+}
--- Programmers/level_0/260106_두_수의_곱_구하기.java
+++ Programmers/level_0/260106_두_수의_곱_구하기.java
@@ -1,4 +1,7 @@
-class Solution {
+package level_0;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/120804
+class Solution120804 {
     public int solution(int num1, int num2) {
         return num1*num2;
     }
