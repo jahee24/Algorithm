4abb9be41c851de73eab94a1d96cf811d95e67c4
jahee24 <8dhfflavlr8@naver.com>
2026-01-08 14:28:34 +0900

    Solve:편지

---
+++ Programmers/level_0/120898_편지.java
@@ -0,0 +1,8 @@
+package level_0;
+//https://school.programmers.co.kr/learn/courses/30/lessons/120898
+class Solution120898 {
+    public int solution(String message) {
+        char[] letter = message.toCharArray();
+        return letter.length * 2;
+    }
+}
